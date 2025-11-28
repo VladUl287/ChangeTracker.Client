08c113015cb11918c2d750fc56927c2297b336d7
User <ulyanovskiy.2001@mail.ru>
2025-11-28 14:42:25 +0300

    refactor: controller

--- Npgsql.EFCore.Tracker.Api.Demo/Controllers/WeatherForecastController.cs
+++ Npgsql.EFCore.Tracker.Api.Demo/Controllers/WeatherForecastController.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using Npgsql.EFCore.Tracker.AspNet.Attributes;
-using Npgsql.EFCore.Tracker.AspNet.Services.Contracts;
 
 namespace Npgsql.EFCore.Tracker.Api.Demo.Controllers;
 
@@ -8,28 +7,20 @@ namespace Npgsql.EFCore.Tracker.Api.Demo.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
+    private static readonly string[] Summaries =
+    [
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
+    ];
 
     [HttpGet(Name = "GetWeatherForecast")]
-    [Track("/weatherforecast/", "Tracker")]
+    [Track("weatherforecast", "Tracker")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 1000).Select(index => new WeatherForecast
+        return [.. Enumerable.Range(1, 1000).Select(index => new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        })];
     }
 }
