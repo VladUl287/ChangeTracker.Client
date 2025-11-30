5ae36b7aba2787f5cce33d354f3dddfcc28a5690
User <ulyanovskiy.2001@mail.ru>
2025-11-30 18:26:19 +0300

    fix: Compiler Error CS0181 Attribute constructor parameter has type, which is not a valid attribute parameter type

--- Tracker.AspNet/Attributes/TrackAttribute.cs
+++ Tracker.AspNet/Attributes/TrackAttribute.cs
@@ -5,12 +5,12 @@ public sealed class TrackAttribute(
       string? route = null,
       string[]? tables = null,
       Type[]? entities = null,
-      TimeSpan? cacheLifeTime = default) : Attribute
+      long cacheLifeTime = default) : Attribute
 {
     public string? Route { get; } = route;
     public string[]? Tables { get; } = tables;
     public Type[]? Entities { get; } = entities;
-    public TimeSpan? CacheLifeTime { get; } = cacheLifeTime;
+    public long CacheLifeTime { get; } = cacheLifeTime;
 
     internal bool IsGlobal => Tables is null or { Length: 0 } && Entities is null or { Length: 0 };
 }
