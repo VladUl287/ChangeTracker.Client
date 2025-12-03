6ab1c0f0595bae3a1956e6a0a4377dcbdaafcaea
User <ulyanovskiy.2001@mail.ru>
2025-12-03 16:24:36 +0300

    refactor: tracker endpoint filter use primary ctor

--- Tracker.AspNet/Services/TrackerEndpointFilter.cs
+++ Tracker.AspNet/Services/TrackerEndpointFilter.cs
@@ -4,22 +4,12 @@ using Tracker.AspNet.Services.Contracts;
 
 namespace Tracker.AspNet.Services;
 
-public sealed class TrackerEndpointFilter : IEndpointFilter
+public sealed class TrackerEndpointFilter(
+    IETagService eTagService, IRequestFilter requestFilter, ImmutableGlobalOptions options) : IEndpointFilter
 {
-    private readonly IETagService _eTagService;
-    private readonly IRequestFilter _requestFilter;
-    private readonly ImmutableGlobalOptions _options;
-
-    public TrackerEndpointFilter(IETagService eTagService, IRequestFilter requestFilter, ImmutableGlobalOptions options)
-    {
-        ArgumentNullException.ThrowIfNull(eTagService, nameof(eTagService));
-        ArgumentNullException.ThrowIfNull(requestFilter, nameof(requestFilter));
-        ArgumentNullException.ThrowIfNull(options, nameof(options));
-
-        _eTagService = eTagService;
-        _requestFilter = requestFilter;
-        _options = options;
-    }
+    private readonly IETagService _eTagService = eTagService ?? throw new NullReferenceException();
+    private readonly IRequestFilter _requestFilter = requestFilter ?? throw new NullReferenceException();
+    private readonly ImmutableGlobalOptions _options = options ?? throw new NullReferenceException();
 
     public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
     {
