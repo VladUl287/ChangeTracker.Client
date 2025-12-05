d822e18df507bd5bc562918b2daf6162bdaed681
User <ulyanovskiy.2001@mail.ru>
2025-12-05 10:43:02 +0300

    feat: GetTypeHashId use short base64 from xxhash64 ulong

--- Tracker.Core/Extensions/TypesExtensions.cs
+++ Tracker.Core/Extensions/TypesExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Security.Cryptography;
+﻿using System.Buffers;
+using System.Buffers.Binary;
+using System.IO.Hashing;
 using System.Text;
 
 namespace Tracker.Core.Extensions;
@@ -8,12 +10,39 @@ public static class TypesExtensions
     public static string GetTypeHashId(this Type type)
     {
         var typeName = type.FullName ?? throw new NullReferenceException();
-        var hashBytes = SHA256.HashData(Encoding.UTF8.GetBytes(typeName));
 
-        var builder = new StringBuilder();
-        foreach (byte b in hashBytes)
-            builder.Append(b.ToString("x2"));
+        var maximumBytes = Encoding.UTF8.GetMaxByteCount(typeName.Length);
 
-        return builder.ToString();
+        const int MaxBytesThreshold = 256;
+        if (maximumBytes > MaxBytesThreshold)
+        {
+            byte[] data = ArrayPool<byte>.Shared.Rent(maximumBytes);
+
+            var count = Encoding.UTF8.GetBytes(typeName, data);
+            var bytes = data.AsSpan()[..count];
+
+            var hash = XxHash64.HashToUInt64(bytes);
+
+            ArrayPool<byte>.Shared.Return(data);
+
+            Span<byte> encodedBytes = stackalloc byte[8];
+            BinaryPrimitives.WriteUInt64LittleEndian(encodedBytes, hash);
+
+            return Convert.ToBase64String(encodedBytes);
+        }
+        else
+        {
+            Span<byte> data = stackalloc byte[maximumBytes];
+
+            var count = Encoding.UTF8.GetBytes(typeName, data);
+            data = data[..count];
+
+            var hash = XxHash64.HashToUInt64(data);
+
+            Span<byte> encodedBytes = stackalloc byte[8];
+            BinaryPrimitives.WriteUInt64LittleEndian(encodedBytes, hash);
+
+            return Convert.ToBase64String(encodedBytes);
+        }
     }
 }
