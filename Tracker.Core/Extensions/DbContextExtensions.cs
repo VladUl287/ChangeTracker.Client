7ea984f6db93fcc5ba38be13c4970083b9c3893d
User <ulyanovskiy.2001@mail.ru>
2025-12-06 21:11:11 +0300

    feat: GetTablesNames add validation

--- Tracker.Core/Extensions/DbContextExtensions.cs
+++ Tracker.Core/Extensions/DbContextExtensions.cs
@@ -6,7 +6,10 @@ public static class DbContextExtensions
 {
     public static IEnumerable<string> GetTablesNames<TContext>(this TContext context, Type[] entities) where TContext : DbContext
     {
-        foreach (var entity in entities ?? [])
+        ArgumentNullException.ThrowIfNull(context, nameof(context));
+        ArgumentNullException.ThrowIfNull(entities, nameof(entities));
+
+        foreach (var entity in entities)
         {
             var entityType = context.Model.FindEntityType(entity) ??
                 throw new NullReferenceException($"Table entity type not found for type {entity.FullName}");
