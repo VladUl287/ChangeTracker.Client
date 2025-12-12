7784365ee8c1b3dd23f04755b0267d5799abdb9a
User <ulyanovskiy.2001@mail.ru>
2025-12-12 10:52:56 +0300

    tests: initlize assembly extensions tests

--- Tracker.Core.Tests/AssemblyExtensionsTests.cs
+++ Tracker.Core.Tests/AssemblyExtensionsTests.cs
@@ -1,5 +1,62 @@
-﻿namespace Tracker.Core.Tests;
+﻿using System.Reflection;
+using System.Reflection.Emit;
+using Tracker.Core.Extensions;
 
-public static class AssemblyExtensionsTests
+namespace Tracker.Core.Tests;
+
+public class AssemblyExtensionsTests
 {
+    [Fact]
+    public void Default_Executing_Assembly()
+    {
+        // Arrange
+        var assembly = Assembly.GetExecutingAssembly();
+
+        // Act
+        var writeTime = assembly.GetAssemblyWriteTime();
+        var lastWriteTimeUtc = File.GetLastWriteTimeUtc(assembly.Location);
+
+        // Assert
+        Assert.Equal(writeTime, lastWriteTimeUtc);
+    }
+
+    [Fact]
+    public void Null_Assembly()
+    {
+        // Arrange
+        var assembly = Assembly.GetExecutingAssembly();
+
+        // Act
+        var writeTime = assembly.GetAssemblyWriteTime();
+        var lastWriteTimeUtc = File.GetLastWriteTimeUtc(assembly.Location);
+
+        // Assert
+        Assert.Equal(writeTime, lastWriteTimeUtc);
+    }
+
+    [Fact]
+    public void Dynamic_Assembly()
+    {
+        // Arrange
+        var assembly = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("NullLocationAssembly"), AssemblyBuilderAccess.RunAndCollect);
+
+        // Act
+        var getWriteTime = () => { assembly.GetAssemblyWriteTime(); };
+
+        // Assert
+        Assert.Throws<ArgumentException>(getWriteTime);
+    }
+
+    [Fact]
+    public void Dynamic_Assembly_W()
+    {
+        // Arrange
+        var assembly = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("NullLocationAssembly"), AssemblyBuilderAccess.RunAndCollect);
+
+        // Act
+        var getWriteTime = () => { assembly.GetAssemblyWriteTime(); };
+
+        // Assert
+        Assert.Throws<ArgumentException>(getWriteTime);
+    }
 }
