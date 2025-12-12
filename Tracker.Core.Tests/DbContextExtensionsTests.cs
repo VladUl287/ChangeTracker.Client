878691cdfd3fa1829832b85710ba802fc462d91d
User <ulyanovskiy.2001@mail.ru>
2025-12-12 10:27:01 +0300

    tests: dbcontext extension tests

--- Tracker.Core.Tests/DbContextExtensionsTests.cs
+++ Tracker.Core.Tests/DbContextExtensionsTests.cs
@@ -1,25 +1,141 @@
-﻿namespace Tracker.Core.Tests;
+﻿using Microsoft.EntityFrameworkCore;
+using Tracker.Core.Extensions;
+
+namespace Tracker.Core.Tests;
 
 public class DbContextExtensionsTests
 {
-    //private sealed class Role(int Id);
+    [Fact]
+    public void Null_Context()
+    {
+        // Arrange
+        Type[] types = [typeof(Role)];
+        EmptyTestDbContext dbContext = null;
 
-    //private sealed class EmptyTestDbContext : DbContext { }
-    //private sealed class TestDbContext : DbContext { 
-    //    public DbSet<Role> Roles { get; init; }
-    //}
-    //private sealed class ConfiguredTestDbContext : DbContext
-    //{
-    //    public DbSet<Role> Roles { get; init; }
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>("context", () =>
+        {
+            dbContext.GetTablesNames(types).ToArray();
+        });
+    }
 
-    //    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    //    {
-    //        base.OnModelCreating(modelBuilder);
+    [Fact]
+    public void Null_Types_Array()
+    {
+        // Arrange
+        Type[] types = null;
+        var dbContext = new EmptyTestDbContext(_dbContextOptions);
 
-    //        modelBuilder.Entity<Role>(builder =>
-    //        {
-    //            //builder.ToTable("roles");
-    //        });
-    //    }
-    //}
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>("entities", () =>
+        {
+            dbContext.GetTablesNames(types).ToArray();
+        });
+    }
+
+    [Fact]
+    public void Empty_Context()
+    {
+        // Arrange
+        Type[] types = [typeof(Role)];
+        var dbContext = new EmptyTestDbContext(_dbContextOptions);
+
+        // Act & Assert
+        Assert.Throws<NullReferenceException>(() =>
+        {
+            dbContext.GetTablesNames(types).ToArray();
+        });
+    }
+
+    [Fact]
+    public void Not_Presented_Entity_Type()
+    {
+        // Arrange
+        Type[] types = [typeof(User)];
+        var dbContext = new TestDbContext(_dbContextOptions);
+
+        // Act & Assert
+        Assert.Throws<NullReferenceException>(() =>
+        {
+            dbContext.GetTablesNames(types).ToArray();
+        });
+    }
+
+    [Fact]
+    public void Presented_Entity_Type()
+    {
+        // Arrange
+        Type[] types = [typeof(Role)];
+        var dbContext = new TestDbContext(_dbContextOptions);
+
+        //Act
+        var tables = dbContext.GetTablesNames(types);
+
+        // Assert
+        Assert.Equal(tables, ["Roles"]);
+    }
+
+    [Fact]
+    public void Configured_Entity_Type()
+    {
+        // Arrange
+        Type[] types = [typeof(Role)];
+        var dbContext = new ConfiguredTestDbContext(_dbContextOptions);
+
+        //Act
+        var tables = dbContext.GetTablesNames(types);
+
+        // Assert
+        Assert.Equal(tables, ["roles"]);
+    }
+
+    [Fact]
+    public void Multiple_Entity_Types()
+    {
+        // Arrange
+        Type[] types = [typeof(Role), typeof(User)];
+        var dbContext = new ConfiguredTestDbContext(_dbContextOptions);
+
+        //Act
+        var tables = dbContext.GetTablesNames(types).ToArray();
+
+        // Assert
+        Assert.Equal(tables, new string[] { "roles", "Users" });
+    }
+
+    private static readonly DbContextOptions _dbContextOptions = new DbContextOptionsBuilder()
+        .UseNpgsql("Host=localhost;Port=5432;Database=main;Username=postgres;Password=postgres")
+        .Options;
+
+    private sealed class Role
+    {
+        public int Id { get; init; }
+    }
+    private sealed class User
+    {
+        public int Id { get; init; }
+    }
+
+    private sealed class EmptyTestDbContext(DbContextOptions options) : DbContext(options) { }
+
+    private sealed class TestDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Role> Roles { get; init; }
+    }
+
+    private sealed class ConfiguredTestDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Role> Roles { get; init; }
+        public DbSet<User> Users { get; init; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Role>(builder =>
+            {
+                builder.ToTable("roles");
+            });
+        }
+    }
 }
