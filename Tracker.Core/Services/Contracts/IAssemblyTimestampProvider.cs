6696a858db152c71c7d397c040620f8998c7f36b
User <ulyanovskiy.2001@mail.ru>
2025-12-19 21:58:18 +0300

    docs: add xml comment to assembly timestamp provider

--- Tracker.Core/Services/Contracts/IAssemblyTimestampProvider.cs
+++ Tracker.Core/Services/Contracts/IAssemblyTimestampProvider.cs
@@ -1,6 +1,15 @@
 ﻿namespace Tracker.Core.Services.Contracts;
 
+/// <summary>
+/// Provides a mechanism to retrieve the timestamp of an assembly.
+/// </summary>
 public interface IAssemblyTimestampProvider
 {
+    /// <summary>
+    /// Gets the last write time of the assembly.
+    /// </summary>
+    /// <returns>
+    /// A <see cref="DateTimeOffset"/> representing the last write time of the assembly.
+    /// </returns>
     DateTimeOffset GetWriteTime();
 }
